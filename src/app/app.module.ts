bc51fe2a24eb10fb8956f168991b5548fab2612a
imkhushboo <vkhushboo540@gmail.com>
2024-04-09 20:33:14 +0530

    created basic form using @NgForm module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { OuterComponent } from './outer/outer.component';
-import { UserAuthModule } from './user-auth/user-auth.module';
-import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    OuterComponent,
-    HeaderComponent
-  ],
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UserAuthModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
