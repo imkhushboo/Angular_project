b373f96ec9f62e68c5d1facb55c815c97f2a2247
imkhushboo <vkhushboo540@gmail.com>
2024-04-13 17:58:17 +0530

    viewchild & content-child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,5 @@ export class AppComponent{
  
   title = "Subject & Behaviour Subject"
 
+
 }
--- src/app/childcomponent/childcomponent.component.ts
+++ src/app/childcomponent/childcomponent.component.ts
@@ -1,29 +1,26 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { DatautilityService } from '../datautility.service';
-
+import { AfterViewInit, AfterContentInit, Component, ElementRef, Renderer2, ViewChild, ContentChildren, QueryList } from '@angular/core';
 
 @Component({
   selector: 'app-childcomponent',
   templateUrl: './childcomponent.component.html',
   styleUrls: ['./childcomponent.component.css']
 })
-export class ChildcomponentComponent implements OnInit{
-  constructor(private _msgService : DatautilityService)
-  {
+export class ChildcomponentComponent implements AfterViewInit, AfterContentInit {
+
+  @ViewChild('box') box!: ElementRef;
+  @ContentChildren('childcomp') childPara!: QueryList<ElementRef>;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    this.renderer.setStyle(this.box.nativeElement, 'backgroundColor', 'red');
   }
 
-  ngOnInit(): void {
-    this._msgService.username.subscribe(username =>  this.user  = username)
+  ngAfterContentInit(): void {
+    console.log(this.childPara);
+    this.childPara.forEach(para => {
+      console.log(para);
+      this.renderer.setStyle(para.nativeElement, 'color', 'white');
+    });
   }
-
-  @Input() boxcolor:string = ""
-  user:any;
-
-
-  updateUser(value:string){
-    this._msgService.username.next(value)
-  }
-
-
-
 }
